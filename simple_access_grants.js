6c4f81dd4416d312fe06972c970ac7a0c8e8411c
Graham-72 <g.oliver@btinternet.com>
2016-05-19 15:17:32 +0100

    Add missing js file.

---
+++ simple_access_grants.js
@@ -0,0 +1,16 @@
+(function ($) {
+
+  Backdrop.behaviors.simple_accessFieldsetSummaries = {
+    attach: function (context) {
+      $('fieldset#edit-simple_access').backdropSetSummary(function (context) {
+        if (!$('.form-checkbox:checked', context).size()) {
+          return Backdrop.t('Public access');
+        }
+        else {
+          return Backdrop.t('Restricted access');
+        }
+      });
+    }
+  };
+
+})(jQuery);
